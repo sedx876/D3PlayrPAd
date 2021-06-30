e00b69fb0cac738828d22fe657ad159f779aaf39
sedx876 <swatkins876@gmail.com>
2021-06-30 09:45:47 -0400

    Added User controller

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,49 @@
+class Api::V1::UsersController < ApplicationController 
+  before_action :set_user, only: [:show, :update, :destroy]
+
+  def index
+    @user = User.all 
+    render json: @users
+  end
+
+  def show
+    user_json = UserSerializer.new(@user).serialized_json
+    render json: user_json
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      render json: UserSerializer.new(@user), status: :created
+    else
+      resp = {error: @user.errors.full_messages.to_sentence}
+      render json: resp, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user.destroy
+  end
+
+  private
+
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    
+    def user_params
+      params.require(:user).permit(:username, :password)
+    end
+
+end
